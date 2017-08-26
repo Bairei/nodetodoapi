2c5bb75f33980c63fe0d00c2bfd3f0bbae72190e
Bairei <baireikawagishi@gmail.com>
2017-08-26 14:42:13 +0200

    Add /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 const {mongoose} = require('./db/mongoose')
 const {User} = require('./models/users')
@@ -22,7 +23,16 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos})
-  }, (e) => {res.status(400).send(e)})
+  }, (e) => res.status(400).send(e))
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  if (!ObjectID.isValid(id)) return res.status(404).send({errorDesc: 'The entered id is not correct!'})
+  Todo.findById(id).then((result) => {
+    if (result) return res.send(result)
+    return res.status(404).send({})
+  }, (e) => res.status(400).send({}))
 })
 
 app.listen(3000, () => {
