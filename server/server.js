51dba1237ef07ab85ce0d113b2a2be71290fcfdc
Bairei <baireikawagishi@gmail.com>
2017-08-26 15:21:14 +0200

    Set up app for heroku deployment

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const {User} = require('./models/users')
 const {Todo} = require('./models/todos')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 app.use(bodyParser.json())
 
 app.post('/todos', (req, res) => {
@@ -35,8 +37,8 @@ app.get('/todos/:id', (req, res) => {
   }, (e) => res.status(400).send({}))
 })
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000!');
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`)
 })
 
 module.exports = {
