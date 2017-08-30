fed3f279c5720ec54b81a612c66ce879ebc58ed8
Bairei <baireikawagishi@gmail.com>
2017-08-30 12:47:52 +0200

    add private GET /users/me route

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb')
 const {mongoose} = require('./db/mongoose')
 const {User} = require('./models/users')
 const {Todo} = require('./models/todos')
+const {authenticate} = require('./middleware/authenticate')
 
 const app = express()
 const port = process.env.PORT
@@ -75,6 +76,10 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     if (e) res.status(400).send(e)
   })
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
 })
 
 app.listen(port, () => {
